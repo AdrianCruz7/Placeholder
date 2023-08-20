0a93ab3da98b741fdc2b5c8862ee0b0663f7502e
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2023-08-20 13:49:49 -0600

    Placehholder
    Placeholders - Val

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField, ] public int test;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
